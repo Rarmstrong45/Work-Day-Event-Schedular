6b4ba1115a1b38b4eb07eaae86c33775f8411a54
Rarmstrong45 <Ryan_armstrong07@yahoo.com>
2021-04-25 11:10:16 -0500

    I used a text input rather that texfield will need to resubmit

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,16 @@
+// Text input class = saveMe id = text(9,10,11,12,1,2,3,4,5)
+// Save Image class = saveIcon id=save(9,10,11,12,1,2,3,4,5)
+
+// create a function that takes data from text input and push it into an Array by clicking "save button" (
+
+// Save Array to local storage 
+
+// retrieve tasks from local storage
+
+// add function to change color due to time
+
+
+// Create Function to display time
 function displayTime() {
 	var today = moment().format('MMMM Do YYYY, h:mm:ss a');
 	$('#currentDay').html(today);
@@ -5,14 +18,7 @@ function displayTime() {
 }
 displayTime();
 
-    function saveTextArea() {
-        var textToWrite = document.getElementById('text9').value;
 
-    $(".save9").on('click', function() {
-        $('#text9').
-    })
-    saveTextArea();
-};
+// I believe I messed up and put a <text input> rather than text area
 
-
-
+// I will come back and redo challenge and re submit.
