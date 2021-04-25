8517fcd7a407a1be278e0486e5c45333c91d81fd
Rarmstrong45 <Ryan_armstrong07@yahoo.com>
2021-04-24 20:54:07 -0500

    Saving current changes

---
+++ Develop/script.js
@@ -0,0 +1,18 @@
+function displayTime() {
+	var today = moment().format('MMMM Do YYYY, h:mm:ss a');
+	$('#currentDay').html(today);
+	setInterval(displayTime, 1000);
+}
+displayTime();
+
+    function saveTextArea() {
+        var textToWrite = document.getElementById('text9').value;
+
+    $(".save9").on('click', function() {
+        $('#text9').
+    })
+    saveTextArea();
+};
+
+
+
